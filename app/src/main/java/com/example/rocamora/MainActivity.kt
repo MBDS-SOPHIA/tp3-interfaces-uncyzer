0c73a7e78e55e29eae2a17af323c19e5830a51ae
uncyzer <uncyzer@gmail.com>
2025-01-06 11:10:04 +0100

    Ajout de la vesion 1, utilsation des images Dice issu de google developper training

--- app/src/main/java/com/example/rocamora/MainActivity.kt
+++ app/src/main/java/com/example/rocamora/MainActivity.kt
@@ -1,47 +1,49 @@
 package com.example.rocamora
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.rocamora.ui.theme.RocamoraTheme
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            RocamoraTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+
+        rollButton.setOnClickListener { rollDice() }
+
+        rollDice()
+    }
+
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Find the ImageView in the layout
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+
+        diceImage.setImageResource(drawableResource)
+
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+class Dice(private val numSides: Int) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RocamoraTheme {
-        Greeting("Android")
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
